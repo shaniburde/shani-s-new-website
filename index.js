5709200b839346f9cad92a9042c622f21be21fbe
Shani Burde <90937179+shaniburde@users.noreply.github.com>
2025-01-26 20:27:43 -0500

    add button index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const headerOne = document.querySelector('h1');
 
+const myButton = document.createElement("button");
+myButton.textContent = "Click me!";
+myButton.addEventListener("click", function() {
+  alert("Button clicked!");
+});
+document.body.appendChild(myButton);
